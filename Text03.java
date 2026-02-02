47ba63fe3d559293ace9e1e8c088c67568745537
ngarusov <ngarusov2@student.cccs.edu>
2026-02-02 12:19:53 -0700

    Add files via upload

---
+++ Text03.java
@@ -0,0 +1,24 @@
+public class Text03 {
+    public static void drawLegs(){
+        System.out.println("     ||     ||      ");
+        System.out.println("     ||     ||      ");
+        System.out.println("    (||)   (||)     ");
+    }
+
+    public static void drawhead(){
+        System.out.println("   /\\         /\\   ");
+        System.out.println("  /  \\_______/  \\  ");
+        System.out.println(" /               \\ ");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)   ");
+    }
+
+    public static void main(String[] args) {
+        drawhead();
+        drawLegs();
+
+    }    
+}
